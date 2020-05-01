487177f9835193ea3b856b59f82ec8d101c8ccc9
stella-loizou <loizou.stella@gmail.com>
2020-05-01 19:36:05 +0100

    Remove ingredients from state

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,19 +7,28 @@ import Search from './Search';
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
   const addIngredientHandler = (ingredient) => {
-    setIngredients((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    setIngredients((prevIngredients) => {
+      return [
+        ...prevIngredients,
+        { id: Math.random().toString(), ...ingredient },
+      ];
+    });
   };
-
+  const removeIngredientHandler = (ingredientId) => {
+    setIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId),
+    );
+  };
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
+        <IngredientList
+          ingredients={ingredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
