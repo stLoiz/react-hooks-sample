e118faee5e659175c498c34144bdd616960a7e91
stella-loizou <loizou.stella@gmail.com>
2020-05-04 10:37:12 +0100

    Store ingredients in firebase/database
    I am using firebase database for this sample project.
    Here I am using the fetch api which is built in to modern browsers and is sending http requests.
     I could use async function however I am using the promise just to show explicitly what we are waiting for  to be done before the setIngredients hook take place.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,12 +7,20 @@ import Search from './Search';
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
   const addIngredientHandler = (ingredient) => {
-    setIngredients((prevIngredients) => {
-      return [
-        ...prevIngredients,
-        { id: Math.random().toString(), ...ingredient },
-      ];
-    });
+    fetch(process.env.REACT_APP_FIREBASE_URL + 'ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => {
+        //it will return the response with an automatically generated unique id
+        return response.json();
+      })
+      .then((responseData) => {
+        setIngredients((prevIngredients) => {
+          return [...prevIngredients, { id: responseData.name, ...ingredient }];
+        });
+      });
   };
   const removeIngredientHandler = (ingredientId) => {
     setIngredients((prevIngredients) =>
