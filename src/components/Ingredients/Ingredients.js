21b0512e1b7c99509da767d6464ed0debb5a5a5a
stella-loizou <loizou.stella@gmail.com>
2020-05-04 17:46:53 +0100

    Use useReducer
    The reason here for using a reducer is because it makes code cleaner as all the updating logic is now in the reducer and then you just dispatch actions. It is a very clear to understand flow how you manage your data.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,17 +1,32 @@
-import React, { useState, useCallback } from 'react';
+import React, { useReducer, useState, useCallback } from 'react';
 
 import ErrorModal from '../UI/ErrorModal';
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter((ing) => ing.id !== action.id);
+    default:
+      throw new Error('Should not get there');
+  }
+};
 const Ingredients = () => {
-  const [ingredients, setIngredients] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [ingredients, dispatch] = useReducer(ingredientReducer, []);
 
+  // const [ingredients, setIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
+
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-    setIngredients(filteredIngredients);
+    // setIngredients(filteredIngredients);
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
   }, []);
 
   const addIngredientHandler = (ingredient) => {
@@ -28,8 +43,12 @@ const Ingredients = () => {
         return response.json();
       })
       .then((responseData) => {
-        setIngredients((prevIngredients) => {
-          return [...prevIngredients, { id: responseData.name, ...ingredient }];
+        // setIngredients((prevIngredients) => {
+        //   return [...prevIngredients, { id: responseData.name, ...ingredient }];
+        // });
+        dispatch({
+          type: 'ADD',
+          ingredient: { id: responseData.name, ...ingredient },
         });
       });
   };
@@ -43,11 +62,12 @@ const Ingredients = () => {
     )
       .then((response) => {
         setIsLoading(false);
-        setIngredients((prevIngredients) =>
-          prevIngredients.filter(
-            (ingredient) => ingredient.id !== ingredientId,
-          ),
-        );
+        // setIngredients((prevIngredients) =>
+        //   prevIngredients.filter(
+        //     (ingredient) => ingredient.id !== ingredientId,
+        //   ),
+        // );
+        dispatch({ type: 'DELETE', id: ingredientId });
       })
       .catch((error) => {
         setIsLoading(false);
