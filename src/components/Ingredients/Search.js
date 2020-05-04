94a16aee5e825212b9b7fe6a066617d42466e1a3
stella-loizou <loizou.stella@gmail.com>
2020-05-04 14:36:42 +0100

    Fix the issue were we are sending request for each keystroke
    We do not want to send request to the server for each keystroke, because we are spamming the server.
    We can set a timer and when the timer expires we check if the current input is the same as the one in the beginning of the timer and only if it is the same we know that there has been a pause and the user paused typing and we want to send the request.
    
    To get the current value of the input we use the useRef() hook. This allows to create a reference and connect the input dom element with that reference by using ref property supported by React.
    
    The variable inputRef should alse be included in the dependency array of useEffect to make sure that it will rerun if the inputRef changes.

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,34 +6,40 @@ import './Search.css';
 const Search = React.memo((props) => {
   const [filterState, setFilterState] = useState('');
   const { onLoadIngredients } = props;
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query =
-      filterState.length === 0
-        ? ''
-        : `?orderBy="title"&equalTo="${filterState}"`;
-    fetch(`${process.env.REACT_APP_FIREBASE_URL}ingredients.json${query}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((responseData) => {
-        const loadIngredients = [];
-        for (const key in responseData) {
-          loadIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    setTimeout(() => {
+      if (filterState === inputRef.current.value) {
+        const query =
+          filterState.length === 0
+            ? ''
+            : `?orderBy="title"&equalTo="${filterState}"`;
+        fetch(`${process.env.REACT_APP_FIREBASE_URL}ingredients.json${query}`)
+          .then((response) => {
+            return response.json();
+          })
+          .then((responseData) => {
+            const loadIngredients = [];
+            for (const key in responseData) {
+              loadIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadIngredients(loadIngredients);
           });
-        }
-        onLoadIngredients(loadIngredients);
-      });
-  }, [filterState, onLoadIngredients]);
+      }
+    }, 500);
+  }, [filterState, onLoadIngredients, inputRef]);
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={filterState}
             onChange={(event) => setFilterState(event.target.value)}
