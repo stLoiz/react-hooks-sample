2889cf1e6bc751dc69a5704bb7523bb668fffde4
stella-loizou <loizou.stella@gmail.com>
2020-05-05 09:34:33 +0100

    Wrap the app with the Provider
    If we want to have access to the context everywhere in the entire app we could wrap the app with the provider.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AuthContextProvider from './context/auth-context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
