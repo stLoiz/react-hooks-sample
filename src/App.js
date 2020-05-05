c6941169b3cece121cf6b5acf50791d2cf511872
stella-loizou <loizou.stella@gmail.com>
2020-05-05 09:36:49 +0100

    Use the useContext () hook to gain access to the context and use the data provided by the context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Ingredients from './components/Ingredients/Ingredients';
+import Auth from './components/Auth';
+import { AuthContext } from './context/auth-context';
 
 const App = (props) => {
-  return <Ingredients />;
+  const authContext = useContext(AuthContext);
+
+  return <>{authContext.isAuth ? <Ingredients /> : <Auth />}</>;
 };
 
 export default App;
